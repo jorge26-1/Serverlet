681646e76295229c3ebc7231b0fa3a31198b36a9
jorgeroa25 <jroa1878@gmail.com>
2025-06-18 08:33:43 -0500

    yya

--- src/java/Controlador/RegistrarHumedadServlet.java
+++ src/java/Controlador/RegistrarHumedadServlet.java
@@ -12,7 +12,7 @@ public class RegistrarHumedadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String tipo = request.getParameter("tipo"); // "huerto" o "corral"
+        String tipo = request.getParameter("tipo");
         String nombre = request.getParameter("nombre");
         String fecha = request.getParameter("fecha");
         String nivelStr = request.getParameter("nivel_humedad");
